4b9e42ae1cfb59ba860fd57bece2720964dabfd7
Matt Braymer-Hayes <braymerhayes@econw.com>
2017-03-22 17:01:07 -0700

    Add model run interface (ModelRun)

--- proforma/run.py
+++ proforma/run.py
@@ -6,6 +6,34 @@ from functools import lru_cache
 from.hbu import HBU
 
 
+class ModelRun:
+    """Model run."""
+
+    def __init__(self, parcels, prototypes, screen):
+        """init."""
+        self.parcels = deepcopy(parcels)
+        self.prototypes = deepcopy(prototypes)
+        self.screen = screen
+
+        self.parcel_runs = [ParcelRun(parcel, prototypes, screen) for parcel in self.parcels]
+
+        self.parcels_new = []
+        for pr in self.parcel_runs:
+            parcel_new = deepcopy(pr.parcel)
+            parcel_new.sf -= pr.n_sf
+            self.parcels_new.append(parcel_new)
+
+    @property
+    def n_sf(self):
+        """Total number of square feet yielded across all parcels."""
+        return sum(pr.n_sf for pr in self.parcel_runs)
+
+    @property
+    def n_units(self):
+        """Total number of units yielded across all parcels."""
+        return sum(pr.n_units for pr in self.parcel_runs)
+
+
 class ParcelRun:
     """Parcel run."""
 
@@ -41,11 +69,11 @@ class ParcelRun:
         return HBU(self.parcel, prototypes, self.screen, prev_hbu=self.hbu_2)
 
     @property
-    def n_units(self):
-        """Total number of units yielded across all highest and best uses."""
-        return sum(hbu.n_units for hbu in (self.hbu_1, self.hbu_2, self.hbu_3))
-
-    @property
     def n_sf(self):
         """Total number of square feet yielded across all highest and best uses."""
         return sum(hbu.n_sf for hbu in (self.hbu_1, self.hbu_2, self.hbu_3))
+
+    @property
+    def n_units(self):
+        """Total number of units yielded across all highest and best uses."""
+        return sum(hbu.n_units for hbu in (self.hbu_1, self.hbu_2, self.hbu_3))
